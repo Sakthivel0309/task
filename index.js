e6228f2e00903339e62648f38d9a2d8000227856
Sakthivel0309 <sakthivelraja3902@gmail.com>
2024-05-29 21:41:40 +0530

    commit

--- index.js
+++ index.js
@@ -325,6 +325,120 @@ document.querySelector(".ques53").addEventListener("click",()=>{
     for(let num=c;num<=d;num++){
          r+=num;
          ki++; 
+         console.log(num);
       }
       document.querySelector(".ques54").innerHTML=r/(ki);
+      
     })  
+  document.querySelector(".oddeven").addEventListener("click",()=>{
+    var f=document.querySelector(".even").value
+    if(f%2==0){
+      document.querySelector(".ansofoddeven").innerHTML+="Even Number"
+    }
+    else{
+      document.querySelector(".ansofoddeven").innerHTML+="odd Number"
+  
+    }
+  })
+  // q--------2
+  document.querySelector(".reverseanumber").addEventListener("click",()=>{
+    var d=document.querySelector(".reverse").value
+    var string=d.split('').reverse().join('')
+    document.querySelector(".reverse1").innerHTML=string
+  })
+  // q---------3
+  document.querySelector(".palinCheck").addEventListener("click",()=>{
+    var palindromeNumber=Number(document.querySelector(".paliInput").value)
+    var temp=palindromeNumber
+    var result=0
+    while(temp>0){
+      var remove=temp%10
+      result=(result*10)+remove
+      temp=Math.floor(temp/10)
+    }
+  if(palindromeNumber==result){
+    var answer1="The Number is Palindrome"
+    document.querySelector(".paliAnswer").innerHTML=answer1
+  }
+  else{
+    var answer2="The Number is Not A Palindrome"
+    document.querySelector(".paliAnswer").innerHTML=answer2
+   
+  }
+  
+  })
+  // q---4
+  document.querySelector(".armCheck").addEventListener("click",()=>{
+    var d=document.querySelector(".Armvalue").value
+    var temp=d
+    var result=0
+    while(temp>0){
+      var lastValue=temp%10
+      result+=lastValue**3
+      temp=Math.floor(temp/10)
+    }
+  if(d==result){
+    var answer1="The number is Armstrong"
+    document.querySelector(".armAnswer").innerHTML=answer1
+  }
+  else{
+    var answer2="The number is not a Armstrong"
+    document.querySelector(".armAnswer").innerHTML=answer2
+  }
+  })
+  // q-----5
+  document.querySelector(".primeCheck").addEventListener("click",()=>{
+    var d=document.querySelector(".primeValue").value
+    if(d<=1){
+      answer1="Not a prime Number"
+      document.querySelector(".primeAnswer").innerHTML=answer1
+    }
+    else{
+      for(var num=2;num<d;num++){
+        if(d%num==0){
+          answer2="prime not Number"
+          document.querySelector(".primeAnswer").innerHTML=answer2
+          break
+      }
+      else{
+        answer3="prime Number"
+          document.querySelector(".primeAnswer").innerHTML=answer3
+          break
+      }
+      }
+    }
+  })
+  // q-------6
+  document.querySelector(".EvenCheck").addEventListener("click",()=>{
+    var d=document.querySelector(".OddEvenValue").value
+    var sumOFEven=0
+    var sumOfOdd=0
+    for(var num=1;num<=d;num++){
+      if(num%2==0){
+        sumOFEven+=num
+      }
+      else{
+        sumOfOdd+=num
+      }
+    }
+    document.querySelector(".sumAnswer").innerHTML+=sumOFEven
+    document.querySelector(".sumAnswer1").innerHTML+=sumOfOdd
+  })
+  // q---7
+  document.querySelector(".oddCheck").addEventListener("click",()=>{
+    var d=document.querySelector(".oddvalue").value
+    for(var num=1;num<=d;num++){
+      if(num%2!=0){
+        document.querySelector(".oddofAnswer").innerHTML+=`${num}${" "}`
+    }
+    }
+  })
+  // q------8
+  document.querySelector(".EnterValueCheck").addEventListener("click",()=>{
+    var d=document.querySelector(".EnterValue").value
+    for(var num=1;num<=d;num++){
+      if(num%10!=5){
+        document.querySelector(".fiveofAnswer").innerHTML+=`${num}${" "}`
+    }
+    }
+  })
